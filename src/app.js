562008415396f2f83ba32dae4dda4ba15628429c
Marcos Souza <bytecore@gmail.com>
2020-10-26 23:53:21 -0300

    First Commit

---
+++ src/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+
+const { v4: uuid } = require('uuid');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  
+  return response.json(repositories);
+  
+});
+
+app.post("/repositories", (request, response) => {
+  
+  const { title, url, techs } = request.body;
+
+  const repositori = { id: uuid(), title, url, techs, like: 0 };
+
+  repositories.push(repositori);
+
+  return response.status(201).json(repositori);
+
+});
+
+app.put("/repositories/:id", (request, response) => {
+  // TODO
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  // TODO
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  // TODO
+});
+
+module.exports = app;
